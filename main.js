7b79ebd3badac731c118444648a209ac48d58c3b
noelcastillon1 <noelcastillon1@gmail.com>
2024-01-16 16:52:37 -0500

    add bilingual alert

---
+++ main.js
@@ -0,0 +1,9 @@
+const alertButton = document.getElementById("close-bilingual");
+
+alertButton.addEventListener("click", () => {
+  document.getElementById("bilingual-alert").style.display = "none";
+});
+
+// document.getElementById("bilingual-alert").addEventListener("click", () => {
+//   document.getElementById("close-bilingual").style.display = "none";
+// });
