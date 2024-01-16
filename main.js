d7b838af4446797bcd8b19986b405c1a88adfc77
noelcastillon1 <noelcastillon1@gmail.com>
2024-01-16 17:03:39 -0500

    add alert button local stoage func

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const alertButton = document.getElementById("close-bilingual");
+const alertBanner = document.getElementById("bilingual-alert");
+
+window.onload = function () {
+  let alertBannerClosed = localStorage.getItem("alertBannerClosed");
+  if (alertBannerClosed === "true") {
+    alertBanner.style.display = "none";
+  }
+};
 
 alertButton.addEventListener("click", () => {
-  document.getElementById("bilingual-alert").style.display = "none";
+  alertBanner.style.display = "none";
+  localStorage.setItem("alertBannerClosed", "true");
 });
-
-// document.getElementById("bilingual-alert").addEventListener("click", () => {
-//   document.getElementById("close-bilingual").style.display = "none";
-// });
