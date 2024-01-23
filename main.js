cb8fa5e5cc9950883111d87473756297ef1a60af
noelcastillon1 <noelcastillon1@gmail.com>
2024-01-22 21:09:19 -0500

    disable local storage for bilingual alert

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const alertButton = document.getElementById("close-bilingual");
 const alertBanner = document.getElementById("bilingual-alert");
 
 window.onload = function () {
-  let alertBannerClosed = localStorage.getItem("alertBannerClosed");
+  // let alertBannerClosed = localStorage.getItem("alertBannerClosed");
   if (alertBannerClosed === "true") {
     alertBanner.style.display = "none";
   }
@@ -32,5 +32,5 @@ window.onload = function () {
 
 alertButton.addEventListener("click", () => {
   alertBanner.style.display = "none";
-  localStorage.setItem("alertBannerClosed", "true");
+  // localStorage.setItem("alertBannerClosed", "true");
 });
